d5cc5ec687e37725f8de6929ced4b6247382684f
Ramazan Ayvaz <ramazanayvaz@msn.com>
2019-01-27 18:00:42 +0300

    Sample code added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
+import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: 'welcome',
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: WelcomeComponent }
+    ]
+  },
+  {
+    path: 'login',
+    component: EmptyLayoutComponent,
+    children: [
+      { path: '', component: LoginComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'welcome' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyLayoutComponent,
+    SiteLayoutComponent,
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
